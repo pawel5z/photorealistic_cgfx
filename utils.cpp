8e09f3b1cc61c5410ea5d6574807b71b3b728b5c
Paweł Szmergała <pawelszme@gmail.com>
2022-03-17 19:38:40 +0100

    Fix includes in `utils.*`

--- utils.cpp
+++ utils.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "utils.hpp"
 
 void ilLogErrorStack() {
--- utils.hpp
+++ utils.hpp
@@ -1,3 +1,9 @@
-#include <IL/devil_cpp_wrapper.hpp>
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <IL/il.h>
+#include <IL/ilu.h>
 
 void ilLogErrorStack();
+
+#endif // UTILS_HPP
