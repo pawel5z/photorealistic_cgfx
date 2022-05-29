363be9a697de0aa119daac6dcba5048d7b36e2ca
Paweł Szmergała <pawelszme@gmail.com>
2022-05-29 23:43:27 +0200

    Implement Moller ray-triangle intersection

--- utils.cpp
+++ utils.cpp
@@ -2,6 +2,7 @@
 #include <IL/ilu.h>
 #include <epoxy/gl.h>
 #include <fstream>
+#include <glm/glm.hpp>
 #include <iostream>
 #include <vector>
 
@@ -120,3 +121,76 @@ void getShaderSource(GLuint sId, const char *file) {
  * AGL3:  GL/GLFW init AGLWindow and AGLDrawable class definitions
  * Ver.3  14.I.2020 (c) A. Łukaszewski
  */
+
+// >>> adapted from https://ii.uni.wroc.pl/~anl/dyd/RGK/files/triangle_moller.c
+#define EPSILON .000001f
+
+bool intersectRayTriangle(const glm::vec3 &orig, const glm::vec3 &dir, const glm::vec3 &vert0,
+                          const glm::vec3 &vert1, const glm::vec3 &vert2, glm::vec2 &baryPosition,
+                          float &t) {
+    glm::vec3 edge1, edge2, tvec, pvec, qvec;
+    float det, inv_det;
+
+    /* find vectors for two edges sharing vert0 */
+    edge1 = vert1 - vert0;
+    edge2 = vert2 - vert0;
+
+    /* begin calculating determinant - also used to calculate U parameter */
+    pvec = glm::cross(dir, edge2);
+
+    /* if determinant is near zero, ray lies in plane of triangle */
+    det = glm::dot(edge1, pvec);
+
+#ifdef TEST_CULL /* define TEST_CULL if culling is desired */
+    if (det < EPSILON)
+        return false;
+
+    /* calculate distance from vert0 to ray origin */
+    tvec = orig - vert0;
+
+    /* calculate U parameter and test bounds */
+    baryPosition[0] = glm::dot(tvec, pvec);
+    if (baryPosition[0] < 0.f || baryPosition[0] > det)
+        return false;
+
+    /* prepare to test V parameter */
+    qvec = glm::cross(tvec, edge1);
+
+    /* calculate V parameter and test bounds */
+    baryPosition[1] = glm::dot(dir, qvec);
+    if (baryPosition[1] < 0.f || baryPosition[0] + baryPosition[1] > det)
+        return false;
+
+    /* calculate t, scale parameters, ray intersects triangle */
+    t = glm::dot(edge2, qvec);
+    inv_det = 1.f / det;
+    t *= inv_det;
+    u *= inv_det;
+    v *= inv_det;
+#else /* the non-culling branch */
+    if (det > -EPSILON && det < EPSILON)
+        return false;
+    inv_det = 1.f / det;
+
+    /* calculate distance from vert0 to ray origin */
+    tvec = orig - vert0;
+
+    /* calculate U parameter and test bounds */
+    baryPosition[0] = glm::dot(tvec, pvec) * inv_det;
+    if (baryPosition[0] < 0.f || baryPosition[0] > 1.f)
+        return false;
+
+    /* prepare to test V parameter */
+    qvec = glm::cross(tvec, edge1);
+
+    /* calculate V parameter and test bounds */
+    baryPosition[1] = glm::dot(dir, qvec) * inv_det;
+    if (baryPosition[1] < 0.f || baryPosition[0] + baryPosition[1] > 1.f)
+        return false;
+
+    /* calculate t, ray intersects triangle */
+    t = glm::dot(edge2, qvec) * inv_det;
+#endif
+    return true;
+}
+// >>> adapted from https://ii.uni.wroc.pl/~anl/dyd/RGK/files/triangle_moller.c
--- utils.hpp
+++ utils.hpp
@@ -1,6 +1,9 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <epoxy/gl.h>
+#include <glm/glm.hpp>
+
 void ilLogErrorStack();
 /**
  * >>>
@@ -17,4 +20,11 @@ int compileProgramFromFile(GLuint &pId, const char *vs, const char *fs, const ch
  * Ver.3  14.I.2020 (c) A. Łukaszewski
  */
 
-#endif // UTILS_HPP
+// >>> adapted from https://ii.uni.wroc.pl/~anl/dyd/RGK/files/triangle_moller.c
+
+bool intersectRayTriangle(const glm::vec3 &orig, const glm::vec3 &dir, const glm::vec3 &vert0,
+                          const glm::vec3 &vert1, const glm::vec3 &vert2, glm::vec2 &baryPosition,
+                          float &t);
+// <<< adapted from https://ii.uni.wroc.pl/~anl/dyd/RGK/files/triangle_moller.c
+
+#endif // !UTILS_HPP
