d2ad7477c40342aac626b636fafb308234a6e61a
Paweł Szmergała <pawelszme@gmail.com>
2022-06-06 22:08:10 +0200

    Add test of triangle area

--- source/test.cpp
+++ source/test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 #include "HemisphereSampler.hpp"
-
+#include "Mesh.hpp"
 #include "utils.hpp"
 
 static void printSamplerResults(HemisphereSampler &&sampler) {
@@ -22,10 +22,17 @@ int main() {
     printSamplerResults(UniformSampler());
     std::cout << '\n';
 
+    std::cout << "Sample rotations" << '\n';
     std::cout << HemisphereSampler::makeSampleRelativeToNormal(glm::vec3(0, 1, 0),
                                                                glm::vec3(1, 0, 0))
               << '\n';
     std::cout << HemisphereSampler::makeSampleRelativeToNormal(
                      glm::vec3(0, glm::sqrt(2.f) / 2.f, glm::sqrt(2.f) / 2.f), glm::vec3(1, 0, 0))
               << '\n';
+    std::cout << '\n';
+
+    std::cout << "Area of triangle" << '\n';
+    std::vector<Vertex> vertices{{{0, 0, 0}}, {{1, 0, 0}}, {{0, 1, 0}}};
+    Triangle trian = {{0, 1, 2}};
+    std::cout << trian.area(vertices) << '\n';
 }
